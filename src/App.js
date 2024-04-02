0d8d3e6d0830bacbffe642f150df644730217b55
george827 <kinyanjuigeorge827@gmail.com>
2024-04-02 12:11:37 +0300

    Reposition the footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
-import Calculator from './components/pages/Calculator';
-import Home from './components/pages/Home';
-import Quote from './components/pages/Quote';
-import './App.css';
+import Calculator from "./components/pages/Calculator";
+import Home from "./components/pages/Home";
+import Quote from "./components/pages/Quote";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -16,17 +16,28 @@ class App extends React.Component {
     return (
       <>
         <nav>
-          <div className="header">
+          <div className='header'>
             <h1 className="title">Math Magicians</h1>
 
             <ul className="navigation">
-              <li><Link to="/" className="home-link">Home</Link></li>
-              <li><Link to="/Calculator">Calculator</Link></li>
-              <li><Link to="/Quote">Quote</Link></li>
+              <li>
+                <Link to="/" className="home-link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/Calculator">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/Quote">Quote</Link>
+              </li>
             </ul>
           </div>
         </nav>
-        <div className="footer"><p>Math Magicians &copy; 2023</p></div>
+        <br />
+        <div className="footer">
+          <p>Math Magicians &copy; 2023</p>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Calculator" element={<Calculator />} />
--- src/components/pages/Quote.js
+++ src/components/pages/Quote.js
@@ -1,13 +1,12 @@
 import React from 'react';
 // eslint-disable-next-line
 const Quote = () => (
-
   <div className="quote">
     <p>
-      &quot;Mathematics is not a careful march down a well-cleared highway,
-      but a journey into a strange wilderness, where the explorers often
-      get lost. Rigor should be a signal to the historians that the maps
-      have been made, and the real explorers have gone elsewhere.&quot; W.S. Anglin
+      &quot;Mathematics is not a careful march down a well-cleared highway, but
+      a journey into a strange wilderness, where the explorers often get lost.
+      Rigor should be a signal to the historians that the maps have been made,
+      and the real explorers have gone elsewhere.&quot; W.S. Anglin
       {' '}
     </p>
   </div>
